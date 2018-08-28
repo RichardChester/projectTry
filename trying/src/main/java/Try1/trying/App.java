c43d0f863c8e6efd368d890df5dd16616262f850
RichardChester <richard.chester93@gmail.com>
2018-08-28 13:43:53 +0100

    trying to work stuff

---
+++ trying/src/main/java/Try1/trying/App.java
@@ -0,0 +1,13 @@
+package Try1.trying;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
